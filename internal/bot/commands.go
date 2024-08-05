f7eccb3aec4429dcffabf584a1cff889e08831a9
tristan-derez <tristan.derez@gmail.com>
2024-08-05 19:19:32 +0200

    add summoner/league implementation

--- internal/bot/commands.go
+++ internal/bot/commands.go
@@ -14,8 +14,8 @@ func (b *Bot) handleInteraction(s *discordgo.Session, i *discordgo.InteractionCr
         b.handleAdd(s, i)
     case "remove":
         b.handleRemove(s, i)
-    case "list":
-        b.handleList(s, i)
+    // case "list":
+    //     b.handleList(s, i)
 	case "ping": 
 		b.handlePing(s, i)
 	}
@@ -28,20 +28,51 @@ func (b *Bot) handleAdd(s *discordgo.Session, i *discordgo.InteractionCreate) {
         return
     }
 
-    summonerName := strings.ToLower(options[0].StringValue())
-	// todo: check on RIOT api if user exists
+    summonerName := strings.TrimSpace(strings.ToLower(options[0].StringValue()))
+    parts := strings.SplitN(summonerName, "#", 2)
+
+    if len(parts) != 2 {
+        log.Printf("Invalid summoner name format: %s", summonerName)
+        respondWithError(s, i, "Invalid summoner name format. Please use Name#Tag.")
+        return
+    }
+
+    gameName := strings.TrimSpace(parts[0])
+    tagLine := strings.TrimSpace(parts[1])
+
+    account, err := b.riotClient.GetAccountPUUIDBySummonerName(gameName, tagLine)
+    if err != nil {
+        log.Printf("Error getting summoner info: %v", err)
+        respondWithError(s, i, "Unable to find summoner.")
+        return
+    }
+
+    summoner, err := b.riotClient.GetSummonerByPUUID(account.SummonerPUUID)
+    if err != nil {
+        log.Printf("Error getting summoner info: %v", err)
+        respondWithError(s, i, "Unable to find summoner.")
+        return
+    }
+
     guildID := i.GuildID
 
-    if err := b.storage.AddSummoner(guildID, summonerName); err != nil {
-        log.Printf("Error adding summoner: %v", err)
-        respondWithError(s, i, "An error occurred while adding the summoner. Please try again later.")
+    rankInfo, err := b.riotClient.GetSummonerRank(summoner.RiotSummonerID)
+    if err != nil {
+        log.Printf("Error fetching summoner rank data: %v", err)
+        respondWithError(s, i, fmt.Sprintf("Error fetching summoner rank data: %v", err))
+        return
+    }
+
+    if err := b.storage.AddSummoner(guildID, summonerName, *summoner, rankInfo); err != nil {
+        log.Printf("Error adding summoner to database: %v", err) // Add this line
+        respondWithError(s, i, fmt.Sprintf("Error adding summoner to database: %v", err)) // Modify this line
         return
     }
 
     s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
         Type: discordgo.InteractionResponseChannelMessageWithSource,
         Data: &discordgo.InteractionResponseData{
-            Content: fmt.Sprintf("Summoner '%s' is now being tracked in this server.", summonerName),
+            Content: fmt.Sprintf("Summoner '%s' (Level %d) is now being tracked in this server.", summonerName, summoner.SummonerLevel),
         },
     })
 }
@@ -70,41 +101,41 @@ func (b *Bot) handleRemove(s *discordgo.Session, i *discordgo.InteractionCreate)
     })
 }
 
-func (b *Bot) handleList(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    guildID := i.GuildID
+// func (b *Bot) handleList(s *discordgo.Session, i *discordgo.InteractionCreate) {
+//     guildID := i.GuildID
 
-    summoners, err := b.storage.ListSummoners(guildID)
-    if err != nil {
-        log.Printf("Error listing summoners: %v", err)
-        respondWithError(s, i, "An error occurred while retrieving the list of summoners. Please try again later.")
-        return
-    }
+//     summoners, err := b.storage.ListSummoners(guildID)
+//     if err != nil {
+//         log.Printf("Error listing summoners: %v", err)
+//         respondWithError(s, i, "An error occurred while retrieving the list of summoners. Please try again later.")
+//         return
+//     }
 
-    var content string
-    if len(summoners) == 0 {
-        content = "No summoners are currently being tracked in this server."
-    } else {
-        content = "Tracked summoners in this server:\n"
-        for _, s := range summoners {
-            rank := "Unknown"
-            if s.Rank != nil {
-                rank = *s.Rank
-            }
-            leaguePoints := 0
-            if s.LeaguePoints != nil {
-                leaguePoints = *s.LeaguePoints
-            }
-            content += fmt.Sprintf("- %s (Rank: %s, LP: %d)\n", s.Name, rank, leaguePoints)
-        }
-    }
+//     var content string
+//     if len(summoners) == 0 {
+//         content = "No summoners are currently being tracked in this server."
+//     } else {
+//         content = "Tracked summoners in this server:\n"
+//         for _, s := range summoners {
+//             rank := "Unknown"
+//             if s.Rank != nil {
+//                 rank = *s.Rank
+//             }
+//             leaguePoints := 0
+//             if s.LeaguePoints != nil {
+//                 leaguePoints = *s.LeaguePoints
+//             }
+//             content += fmt.Sprintf("- %s (Rank: %s, LP: %d)\n", s.Name, rank, leaguePoints)
+//         }
+//     }
 
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: content,
-        },
-    })
-}
+//     s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+//         Type: discordgo.InteractionResponseChannelMessageWithSource,
+//         Data: &discordgo.InteractionResponseData{
+//             Content: content,
+//         },
+//     })
+// }
 
 func (b *Bot) handlePing(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,25 +4,15 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/tristan-derez/league-tracker/internal/config"
+	riotapi "github.com/tristan-derez/league-tracker/internal/riot-api"
 )
 
 type Storage struct {
     db *sql.DB
 }
-
-type Summoner struct {
-    ID           int
-    Name         string
-    Rank         *string
-    LeaguePoints *int
-    CreatedAt    time.Time
-    UpdatedAt    time.Time
-}
-
 
 func New(config *config.Config) (*Storage, error) {
     connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
@@ -53,10 +43,31 @@ func (s *Storage) initDB() error {
         `CREATE TABLE IF NOT EXISTS summoners (
             id SERIAL PRIMARY KEY,
             name TEXT UNIQUE NOT NULL,
-            rank TEXT,
-            league_points INTEGER,
+            riot_account_id TEXT,
+            riot_summoner_id TEXT,
+            riot_summoner_puuid TEXT,
+            summoner_level BIGINT,
+            profile_icon_id INTEGER,
+            revision_date BIGINT,
             created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        )`,
+        `CREATE TABLE league_entries (
+            id SERIAL PRIMARY KEY,
+            summoner_id INTEGER REFERENCES summoners(id),
+            queue_type TEXT NOT NULL,
+            tier TEXT,
+            rank TEXT,
+            league_points INTEGER,
+            wins INTEGER,
+            losses INTEGER,
+            hot_streak BOOLEAN,
+            veteran BOOLEAN,
+            fresh_blood BOOLEAN,
+            inactive BOOLEAN,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            UNIQUE(summoner_id, queue_type)
         )`,
         `CREATE TABLE IF NOT EXISTS guild_summoner_associations (
             guild_id TEXT REFERENCES guilds(guild_id),
@@ -91,22 +102,63 @@ func (s *Storage) AddGuild(guildID, guildName string) error {
     return err
 }
 
-func (s *Storage) AddSummoner(guildID, summonerName string) error {
+func (s *Storage) AddSummoner(guildID, summonerName string, summoner riotapi.Summoner, leagueEntry *riotapi.LeagueEntry) error {
     tx, err := s.db.Begin()
     if err != nil {
-        return err
+        log.Printf("Error beginning transaction: %v", err)
+        return fmt.Errorf("error beginning transaction: %w", err)
     }
     defer tx.Rollback()
 
+    log.Printf("RevisionDate value: %v, Type: %T", summoner.RevisionDate, summoner.RevisionDate)
+
     var summonerID int
     err = tx.QueryRow(`
-        INSERT INTO summoners (name, created_at, updated_at) 
-        VALUES ($1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) 
-        ON CONFLICT (name) DO UPDATE SET updated_at = CURRENT_TIMESTAMP 
-        RETURNING id
-    `, summonerName).Scan(&summonerID)
+    INSERT INTO summoners (
+        name, riot_account_id, riot_summoner_id, riot_summoner_puuid, summoner_level, profile_icon_id, 
+        revision_date, created_at, updated_at
+    ) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7::BIGINT, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) 
+    ON CONFLICT (name) DO UPDATE SET 
+        summoner_level = EXCLUDED.summoner_level,
+        profile_icon_id = EXCLUDED.profile_icon_id,
+        revision_date = EXCLUDED.revision_date,
+        updated_at = CURRENT_TIMESTAMP 
+    RETURNING id
+    `, summonerName, summoner.RiotAccountID, summoner.RiotSummonerID, summoner.SummonerPUUID,
+   summoner.SummonerLevel, summoner.ProfileIconID, summoner.RevisionDate).Scan(&summonerID)
     if err != nil {
-        return err
+        log.Printf("Error inserting/updating summoner: %v", err)
+        return fmt.Errorf("error inserting/updating summoner: %w", err)
+    }
+
+    if leagueEntry != nil {
+        _, err = tx.Exec(`
+            INSERT INTO league_entries (
+                summoner_id, queue_type, tier, rank, league_points,
+                wins, losses, hot_streak, veteran, fresh_blood, inactive,
+                created_at, updated_at
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+            ON CONFLICT (summoner_id, queue_type) DO UPDATE SET
+                tier = EXCLUDED.tier,
+                rank = EXCLUDED.rank,
+                league_points = EXCLUDED.league_points,
+                wins = EXCLUDED.wins,
+                losses = EXCLUDED.losses,
+                hot_streak = EXCLUDED.hot_streak,
+                veteran = EXCLUDED.veteran,
+                fresh_blood = EXCLUDED.fresh_blood,
+                inactive = EXCLUDED.inactive,
+                updated_at = CURRENT_TIMESTAMP
+        `, summonerID, leagueEntry.QueueType, leagueEntry.Tier, leagueEntry.Rank,
+           leagueEntry.LeaguePoints, leagueEntry.Wins, leagueEntry.Losses,
+           leagueEntry.HotStreak, leagueEntry.Veteran, leagueEntry.FreshBlood,
+           leagueEntry.Inactive)
+        if err != nil {
+            log.Printf("Error inserting/updating league_entries: %v", err)
+            return fmt.Errorf("error inserting/updating league_entries: %v", err)
+        }
     }
 
     _, err = tx.Exec(`
@@ -130,33 +182,33 @@ func (s *Storage) RemoveSummoner(guildID, summonerName string) error {
     return err
 }
 
-func (s *Storage) ListSummoners(guildID string) ([]Summoner, error) {
-    rows, err := s.db.Query(`
-        SELECT s.id, s.name, COALESCE(s.rank, ''), COALESCE(s.league_points, 0), s.updated_at
-        FROM summoners s
-        JOIN guild_summoner_associations gsa ON s.id = gsa.summoner_id
-        WHERE gsa.guild_id = $1
-    `, guildID)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+// func (s *Storage) ListSummoners(guildID string) ([]Summoner, error) {
+//     rows, err := s.db.Query(`
+//         SELECT s.id, s.name, COALESCE(s.rank, ''), COALESCE(s.league_points, 0), s.updated_at
+//         FROM summoners s
+//         JOIN guild_summoner_associations gsa ON s.id = gsa.summoner_id
+//         WHERE gsa.guild_id = $1
+//     `, guildID)
+//     if err != nil {
+//         return nil, err
+//     }
+//     defer rows.Close()
 
-    var summoners []Summoner
-    for rows.Next() {
-        var s Summoner
-        var rank string
-        var leaguePoints int
-        if err := rows.Scan(&s.ID, &s.Name, &rank, &leaguePoints, &s.UpdatedAt); err != nil {
-            return nil, err
-        }
-        s.Rank = &rank
-        s.LeaguePoints = &leaguePoints
-        summoners = append(summoners, s)
-    }
+//     var summoners []Summoner
+//     for rows.Next() {
+//         var s Summoner
+//         var rank string
+//         var leaguePoints int
+//         if err := rows.Scan(&s.ID, &s.Name, &rank, &leaguePoints, &s.UpdatedAt); err != nil {
+//             return nil, err
+//         }
+//         s.Rank = &rank
+//         s.LeaguePoints = &leaguePoints
+//         summoners = append(summoners, s)
+//     }
 
-    return summoners, rows.Err()
-}
+//     return summoners, rows.Err()
+// }
 
 func (s *Storage) Close() error {
     return s.db.Close()
