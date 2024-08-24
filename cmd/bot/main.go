26a7d59f19b67d919cf67922952f4690e072b77b
tristan-derez <tristan.derez@gmail.com>
2024-08-24 19:19:10 +0200

    add init function for logs

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"github.com/tristan-derez/league-tracker/internal/bot"
 	"github.com/tristan-derez/league-tracker/internal/config"
 )
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("")
+	log.SetOutput(new(timeWriter))
+}
+
+type timeWriter struct{}
+
+func (tw *timeWriter) Write(p []byte) (n int, err error) {
+	// Get the current time and add 2 hours
+	t := time.Now().Add(2 * time.Hour)
+
+	// Format the time as desired
+	timeStr := t.Format("2006/01/02 15:04:05 ")
+
+	// Write the time and the log message
+	return os.Stdout.Write(append([]byte(timeStr), p...))
+}
 
 func main() {
 	cfg, err := config.Load()
