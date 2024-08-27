fd60de88a2cf3aa4ba2a9486e1996f520b9d47d5
tristan-derez <tristan.derez@gmail.com>
2024-08-28 00:16:35 +0200

    fix: update daily summary job to run at 10:00am

--- internal/bot/daily_summary.go
+++ internal/bot/daily_summary.go
@@ -127,7 +127,7 @@ func (b *Bot) runDailySummaryJob() {
 			}
 			parisTime := utcTime.In(parisLocation)
 
-			if parisTime.Hour() == 00 && (parisTime.Minute() == 15) {
+			if parisTime.Hour() == 10 && (parisTime.Minute() == 00) {
 				log.Println("Running daily summary")
 				b.PublishDailySummary()
 				log.Println("Daily summary completed")
