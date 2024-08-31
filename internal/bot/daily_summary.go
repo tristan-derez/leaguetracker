60c1dc5e0288983dbefa4a1fa6514969207c3355
tristan-derez <tristan.derez@gmail.com>
2024-08-31 12:38:36 +0200

    adjust time as we resolved previous bug

--- internal/bot/daily_summary.go
+++ internal/bot/daily_summary.go
@@ -127,7 +127,7 @@ func (b *Bot) runDailySummaryJob() {
 			}
 			parisTime := utcTime.In(parisLocation)
 
-			if parisTime.Hour() == 12 && (parisTime.Minute() == 35) {
+			if parisTime.Hour() == 10 && (parisTime.Minute() == 00) {
 				log.Println("Running daily summary")
 				b.PublishDailySummary()
 				log.Println("Daily summary completed")
