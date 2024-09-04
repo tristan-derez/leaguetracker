0967fa7eb0e58ea594c635da0263400f7492f550
tristan-derez <tristan.derez@gmail.com>
2024-09-04 10:30:01 +0200

    improve chunk message to respect phrase boundaries

--- internal/utils/chunk_message.go
+++ internal/utils/chunk_message.go
@@ -1,17 +1,34 @@
 package utils
 
-func ChunkMessage(message string, chunkSize int) []string {
-	if len(message) <= chunkSize {
-		return []string{message}
+import "strings"
+
+func ChunkMessage(message string, maxLength int) []string {
+	var chunks []string
+	lines := strings.Split(message, "\n")
+	currentChunk := ""
+
+	for _, line := range lines {
+		if len(currentChunk)+len(line)+1 > maxLength {
+			if currentChunk != "" {
+				chunks = append(chunks, strings.TrimSpace(currentChunk))
+				currentChunk = ""
+			}
+			if len(line) > maxLength {
+				for len(line) > maxLength {
+					chunks = append(chunks, line[:maxLength])
+					line = line[maxLength:]
+				}
+			}
+		}
+		if currentChunk != "" {
+			currentChunk += "\n"
+		}
+		currentChunk += line
 	}
 
-	var chunks []string
-	for start := 0; start < len(message); start += chunkSize {
-		end := start + chunkSize
-		if end > len(message) {
-			end = len(message)
-		}
-		chunks = append(chunks, message[start:end])
+	if currentChunk != "" {
+		chunks = append(chunks, strings.TrimSpace(currentChunk))
 	}
+
 	return chunks
 }
